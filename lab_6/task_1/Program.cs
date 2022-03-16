0573efa6a6dacc0b6eab0cbfd2d9b61885f8bf93
aktivgo <aktivgo01062001@gmail.com>
2022-03-16 10:29:17 +0300

    Init

---
+++ lab_6/task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_1
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine() ?? string.Empty);
+            for (var i = 0; i <= n; i++)
+            {
+                var x = 0;
+                var y = i;
+                while (y >= 0)
+                {
+                    Console.WriteLine($@"Пара: ({x}, {y})");
+                    x++;
+                    y--;
+                }
+            }
+        }
+    }
+}
