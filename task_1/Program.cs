caee15d9f1ffce8e3c148977f6be1bee8bf8ff4b
aktivgo <aktivgo01062001@gmail.com>
2022-03-24 19:41:26 +0300

    Fix task

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -6,7 +6,37 @@ namespace task_1
     {
         public static void Main(string[] args)
         {
-            var n = int.Parse(Console.ReadLine() ?? string.Empty);
+            var input =  Console.ReadLine().Split(' ');
+            
+            var inputX = int.Parse(input[0]);
+            var inputY = int.Parse(input[1]);
+
+            var max = Math.Max(inputX, inputY);
+
+            if (inputX != 0 && inputY != 0)
+            {
+                max += Math.Min(inputX, inputY);
+            }
+            
+            for (var i = 0; i <= max; i++)
+            {
+                var x = 0;
+                var y = i;
+                while (y >= 0)
+                {
+                    Console.WriteLine($"Пара: ({x}, {y})");
+                    
+                    if (x == inputX && y == inputY)
+                    {
+                        return;
+                    }
+                    
+                    x++;
+                    y--;
+                }
+            }
+
+            /*var n = 5;
             for (var i = 0; i <= n; i++)
             {
                 var x = 0;
@@ -17,7 +47,7 @@ namespace task_1
                     x++;
                     y--;
                 }
-            }
+            }*/
         }
     }
 }
